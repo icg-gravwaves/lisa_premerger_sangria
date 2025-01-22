384f472446103c5472e6deae17bb78ab0c1a1a9e
GarethDaviesGW <gareth.cabourndavies@ligo.org>
2025-01-22 16:12:03 +0000

    Start adding work - estimating PSDs

---
+++ datasets/get_sangria_hm.sh
@@ -0,0 +1,6 @@
+# File location:
+# https://lisa-ldc.lal.in2p3.fr/media/uploads/LDC2_sangria_hm_training.h5
+
+if ! [ -f LDC2_sangria_hm_training.h5 ] ; then
+  wget --show-progress https://lisa-ldc.lal.in2p3.fr/media/uploads/LDC2_sangria_hm_training.h5
+fi
