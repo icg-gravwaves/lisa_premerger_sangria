f7d3191ea207d9a30f068058ce43cb18c30f856a
Gareth S Cabourn Davies <gareth.cabourn-davies@port.ac.uk>
2025-11-18 16:02:48 +0000

    work on downloading the data into one place

---
+++ datasets/get_premerger_paper_data.sh
@@ -0,0 +1,20 @@
+# Get stuff from the LISA premerger paper that we are going to reuse in this one
+
+git_url=https://github.com/icg-gravwaves/lisa_premerger_paper/raw/main/
+
+if ! [ -f model_AE_TDI1_SMOOTH_optimistic.txt.gz ] ; then
+    wget --show-progress $git_url/PSD_Files/model_AE_TDI1_SMOOTH_optimistic.txt.gz
+fi
+if ! [ -f model_AE_TDI1_optimistic.txt.gz ] ; then
+    wget --show-progress $git_url/PSD_Files/model_AE_TDI1_optimistic.txt.gz
+fi
+
+if ! [ -f injections.json ] ; then
+    wget --show-progress $git_url/injections.json
+fi
+if ! [ -f signal_0.hdf ] ; then
+    wget --show-progress $git_url/Data/data_files/data_optimistic_psd/signal_0.hdf
+fi
+if ! [ -f signal_zero_noise_0.hdf ] ; then
+    wget --show-progress $git_url/Data/data_files/data_optimistic_psd/signal_zero_noise_0.hdf
+fi
