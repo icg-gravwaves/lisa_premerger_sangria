445e3f46e3b12b10774db158b7a995d9b2eb8617
GarethCabournDavies <gareth.cabourndavies@ligo.org>
2026-02-09 05:38:55 -0800

    get various files from the premerger paper

--- datasets/get_premerger_paper_data.sh
+++ datasets/get_premerger_paper_data.sh
@@ -8,6 +8,18 @@ fi
 if ! [ -f model_AE_TDI1_optimistic.txt.gz ] ; then
     wget --show-progress $git_url/PSD_Files/model_AE_TDI1_optimistic.txt.gz
 fi
+if ! [ -f model_AE_TDI1_SMOOTH_optimistic.txt ] ; then
+    gzip -dk model_AE_TDI1_SMOOTH_optimistic.txt.gz
+fi
+if ! [ -f model_AE_TDI1_optimistic.txt ] ; then
+    gzip -dk model_AE_TDI1_optimistic.txt.gz
+fi
+if ! [ -f model_T_TDI1_optimistic.txt.gz ] ; then
+    wget --show-progress $git_url/PSD_Files/model_T_TDI1_optimistic.txt.gz
+fi
+if ! [ -f model_T_TDI1_optimistic.txt ] ; then
+    gzip -dk model_T_TDI1_optimistic.txt.gz
+fi
 if ! [ -f AE_response_function.txt ] ; then
     wget --show-progress $git_url/PSD_Files/AE_response_function.txt
 fi
@@ -21,6 +33,8 @@ fi
 if ! [ -f signal_zero_noise_0.hdf ] ; then
     wget --show-progress $git_url/Data/data_files/data_optimistic_psd/signal_zero_noise_0.hdf
 fi
-if ! [ -f lisa_ew_1_day_optimistic.hdf ] ; then
-    wget --show-progress $git_url/Search/Template_Banks/lisa_ew_1_day_optimistic.hdf
-fi
+for bank_time_before in 0.5 1 4 7 14 ; do
+    if ! [ -f lisa_ew_${bank_time_before}_day_optimistic.hdf ] ; then
+        wget --show-progress $git_url/Search/Template_Banks/lisa_ew_${bank_time_before}_day_optimistic.hdf
+    fi
+done
